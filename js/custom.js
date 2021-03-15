61bcc9576921bb36539664f89d88fe12f042ac18
yeseo-ko <w44kpo@naver.com>
2021-03-16 06:24:43 +0900

    custom.js안에 각각섹션별 fucntion 없애봄

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 // 상단팝업
 
-    $(function(){
+   
      $('.pop a').click(function(){
     $('.top').hide();   
                 });
-            });
+        
                  
 
 
 
 // 몸통영역
 
-        $(function(){
+       
             $('.main_list').slick({            
                 slidesToShow: 1,
                 autoplay: true,
@@ -21,23 +21,22 @@ $(document).ready(function () {
                 prevArrow:$(".main_left"),
                 nextArrow:$(".main_right")
             });
-        });
+     
 
 
 //  신상품
 
-                $(function(){
                    $('.newjs ol').slick({            
                     slidesToShow: 4,        
                     slidesToSilde: 1,
                      prevArrow:$(".newjs_bt_l"),
                      nextArrow:$(".newjs_bt_r")
                      });
-                });
+           
 
 // 베스트
 
-            $(function(){
+     
                 $(".best ul li").click(function(){
                     $(".best ul li").removeClass("on");
                     $(this).addClass("on");
@@ -48,19 +47,19 @@ $(document).ready(function () {
                     
                 });
                  
-            });
+      
 
 
 // 네일케어 툴
 
-            $(function(){
+     
                $('.tools ol').slick({            
                 slidesToShow: 4,        
                 slidesToSilde: 1,
                  prevArrow:$(".tools_bt_l"),
                  nextArrow:$(".tools_bt_r")
                  });
-            });
+         
 
 
 });
